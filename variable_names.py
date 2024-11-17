39bfe9e6ecb09f2d8b8ee8cfdb4898764dc6bb88
Prakash <prakashy944@gmail.com>
2024-11-17 01:38:23 -0500

    Variables Names Examples
    A variable name must start with a letter or the underscore character
    A variable name cannot start with a number
    A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
    Variable names are case-sensitive (age, Age and AGE are three different variables)
    A variable name cannot be any of the Python keywords.

---
+++ variable_names.py
@@ -0,0 +1,23 @@
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+print(myvar2)
+
+# illegal variable names
+2myvar2 = "John" #should not start with numbers
+my-var = "John" #should not use -
+my var = "John" #should not use spaces
+
+print(my var)
+
+#Camel Case
+myVariableName = "Prakash"
+
+#Pascal Case
+MyVariableName = "Prakash"
+
+#Snake Case
+my_var_name = "Prakash"
